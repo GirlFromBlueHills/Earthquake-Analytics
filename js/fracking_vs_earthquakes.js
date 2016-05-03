fb050d9a8505bea33a549fd0ea87d1daeabb46bc
Jinfa Zhao <jeffzjf@yahoo.com>
2016-05-02 17:21:34 -0700

    Implement zoom in zoom out.

--- js/fracking_vs_earthquakes.js
+++ js/fracking_vs_earthquakes.js
@@ -157,7 +157,19 @@ d3.json("json/us.json", function(error, us) {
                 }
             })
     });
+    // zoom and pan
+    var zoom = d3.behavior.zoom()
+        .on("zoom",function() {
+            g.attr("transform","translate("+
+                                    d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+            g.selectAll("circle")
+                .attr("d", path.projection(projection));
+            g.selectAll("path")
+                .attr("d", path.projection(projection));
 
+        });
+
+    svg.call(zoom)
 
 });
 
